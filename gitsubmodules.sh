58f456215605e43d9aca0b4a273f412eaf59a007
Meowbutu <mewmewdevart@gmail.com>
2022-12-08 18:45:28 -0300

    submodules notes

---
+++ gitsubmodules.sh
@@ -0,0 +1,11 @@
+# Add a Git Submodule
+
+$ git submodule add <remote_url> 
+$ git commit -m "Added the submodule to the project."
+$ git push
+
+# Pull a Git Submodule
+
+$ git submodule update --init --recursive
+
+Link: https://devconnected.com/how-to-add-and-update-git-submodules/
